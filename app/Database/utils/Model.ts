9f7539a5ce21f3f0d5daf1239cf11e5cf551bd76
prototype2010 <alex.zakorko2015@gmail.com>
2019-12-19 14:57:33 +0200

    feature:  Model

---
+++ app/Database/utils/Model.ts
@@ -0,0 +1,36 @@
+import {IndexedObject, Nullable, Table} from "../../types";
+import {DatabaseConnection} from "../Database";
+
+export class Model {
+    protected _dbID: Nullable<number> = null;
+
+    async insertToDB (tableName : Table, insertData : IndexedObject) {
+
+        try {
+
+            const [id] = await DatabaseConnection
+                .getInstance()
+                (tableName)
+                .insert({
+                    id : null,
+                    ...insertData
+                });
+
+            this.dbID = id;
+        } catch (e) {
+            console.error(`Failed to insert data to ${tableName}`);
+            console.error(`Insert data ${JSON.stringify(insertData)}`, e);
+            console.error(`Original error`, e);
+        }
+    }
+
+    get dbID(): Nullable<number> {
+        return this._dbID;
+    }
+
+    set dbID(value: Nullable<number>) {
+        this._dbID = value;
+    }
+
+
+}
