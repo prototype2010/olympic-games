57f4cbeca859c0a705cd4b5477a25f3077d4cf6f
prototype2010 <alex.zakorko2015@gmail.com>
2020-01-10 10:25:22 +0200

    refactoring: console.logs removal

--- config/index.ts
+++ config/index.ts
@@ -3,10 +3,8 @@ export const { DB_FILE_PATH, CSV_FILE_PATH } = require('dotenv').config({
 
   path: (() => {
     if (process.env.test) {
-      console.log('Test env used');
       return '.env.test';
     } else {
-      console.log('Prod env used');
       return '.env';
     }
   })(),
