2381e8429ee4ed78493b6e419a06b20c6d7fb164
prototype2010 <alex.zakorko2015@gmail.com>
2020-01-13 14:13:24 +0200

    feature: build removal

--- build/app/testUtils/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.wrapThrowable = (funcThatThrows, args) => {
-  /* */
-  return function() {
-    funcThatThrows.apply(null, args);
-  };
-};
-//# sourceMappingURL=index.js.map
