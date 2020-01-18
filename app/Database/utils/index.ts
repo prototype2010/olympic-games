b212e72054010491f7cf4172ce6edf0ae235467a
prototype2010 <Firstday2010>
2020-01-18 21:59:05 +0200

    refactoring : complicated inline interface -> Array<Model>

--- app/Database/utils/index.ts
+++ app/Database/utils/index.ts
@@ -4,7 +4,7 @@ import { Table } from '../../types';
 import { DatabaseConnection } from '../Database';
 export const CHUNK_SIZE = 100;
 
-export const resolveAllAsChunks = async (entities: Array<{ write(): Promise<Model> }>) => {
+export const resolveAllAsChunks = async (entities: Array<Model>) => {
   for await (const chunkPart of chunk(entities, CHUNK_SIZE)) {
     await Promise.all(chunkPart.map(entity => entity.write()));
   }
